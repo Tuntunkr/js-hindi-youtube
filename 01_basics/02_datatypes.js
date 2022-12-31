fa9eb1b713692c52cd8b11f0cc2cdb96f8dd2e00
Tuntun <74050853+Tuntunkr@users.noreply.github.com>
2022-12-31 11:42:56 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+// "use strict";  //treat all s code as newer version
+
+//alert(2+2) //we are using nodejs, not browser
+
+// console.log(2
+//     +
+//     3); //code readability should be high
+
+    console.log(typeof "Tuntun");
+
+let name="raja" 
+let age=23
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null=>standalone value
+// undefine=>
+// symbol=> unique
+
+// Object 
+
+
+console.log(typeof null);
+console.log(typeof undefined)
